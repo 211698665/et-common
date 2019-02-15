93ecc928d5d9091057f0926ce2dde9e4db47169d
211698665 <47618129+211698665@users.noreply.github.com>
2019-02-15 21:47:43 +0800

    et-common

---
+++ src/main/java/cn/henu/common/pojo/EasyUIDataGridResult.java
@@ -0,0 +1,23 @@
+package cn.henu.common.pojo;
+
+import java.io.Serializable;
+import java.util.List;
+//用于easyui返回商品列表
+public class EasyUIDataGridResult  implements Serializable{
+
+	private long total;
+	private List rows;
+	public long getTotal() {
+		return total;
+	}
+	public void setTotal(long total) {
+		this.total = total;
+	}
+	public List getRows() {
+		return rows;
+	}
+	public void setRows(List rows) {
+		this.rows = rows;
+	}
+	
+}
---
+++ src/main/java/cn/henu/common/utils/EtResult.java
@@ -0,0 +1,151 @@
+package cn.henu.common.utils;
+
+import java.io.Serializable;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * 易天商城自定义响应结构
+ */
+public class EtResult implements Serializable{
+
+    // 定义jackson对象
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    // 响应业务状态
+    private Integer status;
+
+    // 响应消息
+    private String msg;
+
+    // 响应中的数据
+    private Object data;
+
+    public static EtResult build(Integer status, String msg, Object data) {
+        return new EtResult(status, msg, data);
+    }
+
+    public static EtResult ok(Object data) {
+        return new EtResult(data);
+    }
+
+    public static EtResult ok() {
+        return new EtResult(null);
+    }
+
+    public EtResult() {
+
+    }
+
+    public static EtResult build(Integer status, String msg) {
+        return new EtResult(status, msg, null);
+    }
+
+    public EtResult(Integer status, String msg, Object data) {
+        this.status = status;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public EtResult(Object data) {
+        this.status = 200;
+        this.msg = "OK";
+        this.data = data;
+    }
+
+//    public Boolean isOK() {
+//        return this.status == 200;
+//    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    /**
+     * 将json结果集转化为TaotaoResult对象
+     * 
+     * @param jsonData json数据
+     * @param clazz TaotaoResult中的object类型
+     * @return
+     */
+    public static EtResult formatToPojo(String jsonData, Class<?> clazz) {
+        try {
+            if (clazz == null) {
+                return MAPPER.readValue(jsonData, EtResult.class);
+            }
+            JsonNode jsonNode = MAPPER.readTree(jsonData);
+            JsonNode data = jsonNode.get("data");
+            Object obj = null;
+            if (clazz != null) {
+                if (data.isObject()) {
+                    obj = MAPPER.readValue(data.traverse(), clazz);
+                } else if (data.isTextual()) {
+                    obj = MAPPER.readValue(data.asText(), clazz);
+                }
+            }
+            return build(jsonNode.get("status").intValue(), jsonNode.get("msg").asText(), obj);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    /**
+     * 没有object对象的转化
+     * 
+     * @param json
+     * @return
+     */
+    public static EtResult format(String json) {
+        try {
+            return MAPPER.readValue(json, EtResult.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Object是集合转化
+     * 
+     * @param jsonData json数据
+     * @param clazz 集合中的类型
+     * @return
+     */
+    public static EtResult formatToList(String jsonData, Class<?> clazz) {
+        try {
+            JsonNode jsonNode = MAPPER.readTree(jsonData);
+            JsonNode data = jsonNode.get("data");
+            Object obj = null;
+            if (data.isArray() && data.size() > 0) {
+                obj = MAPPER.readValue(data.traverse(),
+                        MAPPER.getTypeFactory().constructCollectionType(List.class, clazz));
+            }
+            return build(jsonNode.get("status").intValue(), jsonNode.get("msg").asText(), obj);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+}
